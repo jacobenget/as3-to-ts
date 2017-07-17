0d2e336e4c2485b55bc64c4eb25d683832b95c07
Jacob Enget <jenget@nerdery.com>
2017-07-17 17:39:11 -0500

    Allow comments to appear:
    - after to the first open curly bracket of an object literal
    - after a comma that is separating entries in an object literal
    - between the name of a field in an object literal and the ':' following the name
    - between the ':' and the expression providing the value of this object field

--- src/parse/parse-literals.ts
+++ src/parse/parse-literals.ts
@@ -4,6 +4,8 @@ import * as Operators from '../syntax/operators';
 import AS3Parser, {nextToken, consume, skip, tokIs} from "./parser";
 import {parseExpression} from "./parse-expressions";
 import {parseType} from "./parse-types";
+import {MULTIPLE_LINES_COMMENT} from './parser';
+import {startsWith} from '../string';
 
 
 export function parseArrayLiteral(parser:AS3Parser):Node {
@@ -17,13 +19,21 @@ export function parseArrayLiteral(parser:AS3Parser):Node {
     return result;
 }
 
+export function skipAllDocumentation(parser : AS3Parser) : void {
+    if (startsWith(parser.tok.text, MULTIPLE_LINES_COMMENT)) {
+        nextToken(parser, true);
+    }
+}
 
 export function parseObjectLiteral(parser:AS3Parser):Node {
     let tok = consume(parser, Operators.LEFT_CURLY_BRACKET);
+    skipAllDocumentation(parser);
+
     let result:Node = createNode(NodeKind.OBJECT, {start: tok.index, end: tok.end});
     while (!tokIs(parser, Operators.RIGHT_CURLY_BRACKET)) {
         result.children.push(parseObjectLiteralPropertyDeclaration(parser));
         skip(parser, Operators.COMMA);
+        skipAllDocumentation(parser);
     }
     tok = consume(parser, Operators.RIGHT_CURLY_BRACKET);
     result.end = tok.end;
@@ -35,8 +45,9 @@ function parseObjectLiteralPropertyDeclaration(parser:AS3Parser):Node {
     let result:Node = createNode(NodeKind.PROP, {start: parser.tok.index, end: parser.tok.end});
     let name:Node = createNode(NodeKind.NAME, {tok: parser.tok});
     result.children.push(name);
-    nextToken(parser); // name
+    nextToken(parser, true); // name, ignore all trailing documentation
     consume(parser, Operators.COLUMN);
+    skipAllDocumentation(parser);
     let expr = parseExpression(parser);
     let val = createNode(NodeKind.VALUE, {start: parser.tok.index, end: expr.end}, expr);
     result.children.push(val);
