22e28ed09c1494b2127b67d6c0b461bff6a4111e
Jacob Enget <jenget@nerdery.com>
2017-07-26 07:40:55 -0500

    Have the package expose the 'NodeKind' enum and the 'parse' function so:
     - scripts like the one in 'bin' can be written easily from the outside of this package
     - node-visitors can be implemented from the outside as well

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 /*jshint node:true*/
 
 var Parser = require('./lib/parse/parser'),
+    parse = require('./lib/parse'),
     Scanner = require('./lib/parse/scanner'),
     Emitter = require('./lib/emit/emitter'),
     KeyWords = require('./lib/syntax/keywords'),
-    Operators = require('./lib/syntax/operators');
+    Operators = require('./lib/syntax/operators'),
+    NodeKind = require('./lib/syntax/nodeKind');
+
+Parser.parse = parse;
 
 module.exports = {
     Parser: Parser,
     Scanner: Scanner,
     Emitter: Emitter,
     KeyWords: KeyWords,
-    Operators: Operators
+    Operators: Operators,
+    NodeKind: NodeKind
 };
 
 
