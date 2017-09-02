2c42f11c65b3eafbdbe21acf1ed070281a8c59d6
Jacob Enget <jacob.enget@gmail.com>
2017-09-02 01:35:43 -0500

    Fixed skip/catchup behavior of ASSIGN emitter for E4X,
    resulting in correct placement of the trailing ')' of the 'put' expression

--- src/custom-visitors/e4x/assignment.ts
+++ src/custom-visitors/e4x/assignment.ts
@@ -1,5 +1,7 @@
 import Node from '../../syntax/node';
+import * as Operators from '../../syntax/operators'
 import Emitter, { visitNode } from '../../emit/emitter';
+import * as assert from 'assert';
 
 import { isAccessor, isXMLRoot } from './lib';
 
@@ -9,17 +11,26 @@ export const state = {
 
 export default function(emitter: Emitter, node: Node) {
     const lhs = node.children[0];
+    const op = node.children[1];
     const rhs = node.children[2];
     if (isAccessor(lhs) && isXMLRoot(emitter, lhs)) {
+        assert(op.text === Operators.EQUAL);    // we have no logic yet to handle expressions like '+=' when the rhs is XML/XMLList, so assert if something like this is encountered
+        
         state.inAssignment += 1;
 
         visitNode(emitter, lhs);
 
         state.inAssignment -= 1;
-        emitter.insert(', ');
-        emitter.skipTo(rhs.start);
+        
+        emitter.catchup(op.start);
+        // remove any trailing whitespace, so the comma inserted below directly follows the closest non-whitespace character before it
+        emitter.output = emitter.output.replace(/\s+$/, '');
+        emitter.insert(',');
+        emitter.skipTo(op.end);
+        
         visitNode(emitter, rhs);
 
+        emitter.catchup(node.end);
         emitter.insert(')');
 
         return true;
